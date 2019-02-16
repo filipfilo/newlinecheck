4b3501954681437438877538df039bcafcca9691
filipfilo <40231442+filipfilo@users.noreply.github.com>
2019-02-16 11:28:58 +0100

    Update resrt.py

--- resrt.py
+++ resrt.py
@@ -1,21 +1,21 @@
 import re
 
-filename = input('Insert full file name: \n')
+filename = input('Insert full file name: ')
 
 bad_newline_regex = r'\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}\n\s'
 illegal_chars = '<'
 
 def open_srt():
+	"""opens and returns content of input file"""
 	with open(filename, 'r') as f:
 		f_contents = f.read()
 		return f_contents
 		
 def find_wrong_newline():
+	"""looks for regex from line 5 and prints results"""
 	pattern = re.compile(bad_newline_regex)
 	matches = pattern.findall(open_srt())
-	NumberOfErrors = len(matches)
-	sentence = f'Number of newline errors: {NumberOfErrors}\n'
-	print(sentence)
+	print(f'Number of newline errors: {len(matches)}\n')
 	for item in matches:
 		print(item)
 
@@ -26,6 +26,3 @@ def contains_illegal_chars():
 		print(f'does not contain: {illegal_chars}')
 
 find_wrong_newline(), contains_illegal_chars()
-
-
-
